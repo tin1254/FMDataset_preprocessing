58a72709ef30abd8cb0ccac3669bba9284e664e7
tin1254 <tin-1254@hotmail.com>
2020-12-10 03:17:07 +0100

    inverse projection path

--- preprocess.cpp
+++ preprocess.cpp
@@ -3,6 +3,7 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgcodecs.hpp>
+#include "opencv2/imgproc.hpp"
 #include <opencv2/opencv.hpp>
 #include <string>
 
@@ -21,32 +22,19 @@
  *
  *
  *  Depth:
- *  1. Unproject distorted pixel onto image plane (Unproject the coordinate of
- *the color image)
- *  2. Apply transformation color->depth
- *  3. Project the points into distorted pixel (depth image)
- *  4. Bilinear interpolate the value with the depth image at the projected
- *pixel location
- *  5. Fill in the interpolated value into the corresponding location of the
- *pixel at step 1
- *
- *
- *  Color (TODO):
- *  Verify if the result of undistortPoints in opencv is the same as Inverse
- *  Brown-Conrady in realsense
+ *  1. From the depth image, unproject distorted pixels with depth onto image plane 
+ *  2. Apply transformation depth->color
+ *  3. Project the points into distorted pixel (color image)
+ *  4. Fill in the value of the depth image pixel to the corresponding projected pixel of the color image
+ *  5. Smooth the image or interpolation the points, I chose soomthing
  *
  *    Reference:
  *    https://github.com/IntelRealSense/librealsense/wiki/Projection-in-RealSense-SDK-2.0#intrinsic-camera-parameters
  *    https://docs.opencv.org/3.4/da/d54/group__imgproc__transform.html#ga55c716492470bfe86b0ee9bf3a1f0f7e
  **/
 
-// const std::string DATASET_PATH =
-//     "/home/tin/Datasets/FMDataset/dorm1/dorm1_slow/";
-// const std::string OUTPUT_PATH =
-//     "/home/tin/Datasets/FMDataset/dorm1/dorm1_slow/aligned/";
-
-
 const size_t IMAGE_WIDTH = 640, IMAGE_HEIGHT = 480;
+const float DEPTH_SCALE = 1000;
 
 float R_c_d_raw[3][3] = {{0.999980211, -0.00069964811, -0.0062491186},
                          {0.000735448, 0.999983311, 0.00572841847},
@@ -62,19 +50,22 @@ const std::vector<float> dist_coeffs = {0.0644, -0.114, 0.00127, 0.00203, 0};
 void LoadImages(const std::string &strFile,
                 std::vector<std::string> &vstrRGBImageFilenames,
                 std::vector<std::string> &vstrDepthImageFilenames,
-                std::vector<int> &vTimestamps) {
+                std::vector<int> &vTimestamps)
+{
   std::ifstream f;
   f.open(strFile.c_str());
 
   std::string s;
-  getline(f, s);  // skip the first line
-  while (!f.eof()) {
+  getline(f, s); // skip the first line
+  while (!f.eof())
+  {
     getline(f, s);
     std::stringstream ss(s);
 
     std::string time, rgb, depth;
     getline(ss, time, ',');
-    if (time.empty()) break;
+    if (time.empty())
+      break;
     vTimestamps.push_back(std::stoi(time));
     getline(ss, rgb, ',');
     vstrRGBImageFilenames.push_back(rgb);
@@ -88,7 +79,8 @@ void LoadImages(const std::string &strFile,
 void UnprojectDistortedPixelToPoint(cv::Mat &point, const cv::Mat &K,
                                     const std::vector<float> &coeffs,
                                     const size_t pixel_x, const size_t pixel_y,
-                                    float depth) {
+                                    const float depth)
+{
   const auto &fx = K.at<float>(0, 0);
   const auto &fy = K.at<float>(1, 1);
   const auto &ppx = K.at<float>(0, 2);
@@ -97,13 +89,6 @@ void UnprojectDistortedPixelToPoint(cv::Mat &point, const cv::Mat &K,
   float x = (pixel_x - ppx) / fx;
   float y = (pixel_y - ppy) / fy;
 
-  float r2 = x * x + y * y;
-  float f = 1 + coeffs[0] * r2 + coeffs[1] * r2 * r2 + coeffs[4] * r2 * r2 * r2;
-  float ux = x * f + 2 * coeffs[2] * x * y + coeffs[3] * (r2 + 2 * x * x);
-  float uy = y * f + 2 * coeffs[3] * x * y + coeffs[2] * (r2 + 2 * y * y);
-  x = ux;
-  y = uy;
-
   point = (cv::Mat_<float>(3, 1) << depth * x, depth * y, depth);
 }
 
@@ -111,7 +96,8 @@ void UnprojectDistortedPixelToPoint(cv::Mat &point, const cv::Mat &K,
 // https://github.com/IntelRealSense/librealsense/blob/5e73f7bb906a3cbec8ae43e888f182cc56c18692/include/librealsense2/rsutil.h#L15
 void ProjectPointToDistortedPixel(const cv::Mat &point, const cv::Mat &K,
                                   const std::vector<float> &coeffs,
-                                  cv::Point2f &pixel) {
+                                  cv::Point2f &pixel)
+{
   const auto &px = point.at<float>(0);
   const auto &py = point.at<float>(1);
   const auto &pz = point.at<float>(2);
@@ -123,28 +109,34 @@ void ProjectPointToDistortedPixel(const cv::Mat &point, const cv::Mat &K,
 
   float x = px / pz, y = py / pz;
 
-  // float r2 = x * x + y * y;
-  // float f = 1 + coeffs[0] * r2 + coeffs[1] * r2 * r2 + coeffs[4] * r2 * r2 *
-  // r2; x *= f; y *= f; float dx = x + 2 * coeffs[2] * x * y + coeffs[3] * (r2
-  // + 2 * x * x); float dy = y + 2 * coeffs[3] * x * y + coeffs[2] * (r2 + 2 *
-  // y * y); x = dx; y = dy;
+  float r2 = x * x + y * y;
+  float f = 1 + coeffs[0] * r2 + coeffs[1] * r2 * r2 + coeffs[4] * r2 * r2 * r2;
+  x *= f;
+  y *= f;
+  float dx = x + 2 * coeffs[2] * x * y + coeffs[3] * (r2 + 2 * x * x);
+  float dy = y + 2 * coeffs[3] * x * y + coeffs[2] * (r2 + 2 * y * y);
+  x = dx;
+  y = dy;
 
   pixel = cv::Point2f(x * fx + ppx, y * fy + ppy);
 }
 
-void UnprojectPixel(const size_t &cols, const size_t &rows, const cv::Mat &K,
+void UnprojectPixel(const cv::Mat &imD, const cv::Mat &K,
                     const std::vector<float> &dist_coeffs,
                     std::vector<cv::Mat> &vp3D,
-                    std::vector<std::pair<size_t, size_t>> &vpPixelIdx) {
+                    std::vector<std::pair<size_t, size_t>> &vpPixelIdx)
+{
   vp3D.clear();
   vpPixelIdx.clear();
-  vp3D.reserve(cols * rows);
-  vpPixelIdx.reserve(cols * rows);
+  vp3D.reserve(imD.size[0] * imD.size[1]);
+  vpPixelIdx.reserve(imD.size[0] * imD.size[1]);
 
-  for (size_t c = 0; c < cols; ++c) {
-    for (size_t r = 0; r < rows; ++r) {
+  for (size_t c = 0; c < imD.size[1]; ++c)
+  {
+    for (size_t r = 0; r < imD.size[0]; ++r)
+    {
       cv::Mat p3D;
-      UnprojectDistortedPixelToPoint(p3D, K, dist_coeffs, c, r, 1);
+      UnprojectDistortedPixelToPoint(p3D, K, dist_coeffs, c, r, imD.at<ushort>(r, c) / DEPTH_SCALE);
       vp3D.push_back(p3D);
       vpPixelIdx.emplace_back(r, c);
     }
@@ -153,38 +145,32 @@ void UnprojectPixel(const size_t &cols, const size_t &rows, const cv::Mat &K,
 
 void ProjectPoint(const std::vector<cv::Mat> &vp3D, const cv::Mat &K,
                   const std::vector<float> &dist_coeffs,
-                  std::vector<cv::Point2f> &vp2D) {
+                  std::vector<cv::Point2f> &vp2D)
+{
   vp2D.clear();
   vp2D.reserve(vp3D.size());
-  for (const auto &p3D : vp3D) {
+  for (const auto &p3D : vp3D)
+  {
     cv::Point2f p2D;
     ProjectPointToDistortedPixel(p3D, K, dist_coeffs, p2D);
     vp2D.push_back(p2D);
   }
 }
 
-// Reference:
-// https://stackoverflow.com/questions/13299409/how-to-get-the-image-pixel-at-real-locations-in-opencv
-ushort InterpolatePixelValue(const cv::Mat &img, const cv::Point2f &p) {
-  if (!(0 < p.x || p.x >= img.size[0]) && !(0 < p.y || p.y >= img.size[1]))
-    return 0.;
-
-  cv::Mat patch;
-  cv::getRectSubPix(img, cv::Size(1, 1), p, patch);
-  return static_cast<ushort>(patch.at<float>(0));
-}
-
 void Transform3DPoints(const cv::Mat &R, const cv::Mat &t,
-                       std::vector<cv::Mat> &vp3D) {
-  for (auto &p : vp3D) p = R * p + t;
+                       std::vector<cv::Mat> &vp3D)
+{
+  for (auto &p : vp3D)
+    p = R * p + t;
 }
 
-void ProjectColorToDepth(const cv::Mat &K_c, const cv::Mat &K_d,
+void ProjectDepthToColor(const cv::Mat &imD, const cv::Mat &K_c, const cv::Mat &K_d,
                          const std::vector<float> &dist_coeffs,
                          const cv::Mat &R, const cv::Mat &t,
                          std::vector<cv::Mat> &vp3D,
                          std::vector<std::pair<size_t, size_t>> &vpPixelIdx,
-                         std::vector<cv::Point2f> &vp2D) {
+                         std::vector<cv::Point2f> &vp2D)
+{
   vp3D.clear();
   vpPixelIdx.clear();
   vp2D.clear();
@@ -192,32 +178,32 @@ void ProjectColorToDepth(const cv::Mat &K_c, const cv::Mat &K_d,
   vpPixelIdx.reserve(IMAGE_WIDTH * IMAGE_HEIGHT);
   vp2D.reserve(IMAGE_WIDTH * IMAGE_HEIGHT);
 
-  UnprojectPixel(IMAGE_WIDTH, IMAGE_HEIGHT, K_c, dist_coeffs, vp3D, vpPixelIdx);
+  UnprojectPixel(imD, K_d, dist_coeffs, vp3D, vpPixelIdx);
   Transform3DPoints(R, t, vp3D);
-
-  ProjectPoint(vp3D, K_d, dist_coeffs, vp2D);
+  ProjectPoint(vp3D, K_c, dist_coeffs, vp2D);
 }
 
 void AlignDepth(const cv::Mat &imD_original, cv::Mat &imD_aligned,
                 const std::vector<cv::Mat> &vp3D,
                 const std::vector<std::pair<size_t, size_t>> &vpPixelIdx,
-                const std::vector<cv::Point2f> &vp2D) {
-  cv::Mat fImD;
-  imD_original.convertTo(fImD, CV_32F);
+                const std::vector<cv::Point2f> &vp2D)
+{
   imD_aligned = cv::Mat(IMAGE_HEIGHT, IMAGE_WIDTH, CV_16UC1, cv::Scalar(0));
 
-  for (size_t i = 0; i < vp3D.size(); ++i) {
-    const size_t &r = vpPixelIdx[i].first;
-    const size_t &c = vpPixelIdx[i].second;
-    if (r >= 0 && r < imD_original.size[1] && c >= 0 &&
-        c < imD_original.size[01]) {
-      imD_aligned.at<ushort>(r, c) = InterpolatePixelValue(fImD, vp2D[i]);
-    }
+  for (size_t i = 0; i < vp3D.size(); ++i)
+  {
+    const size_t c = std::round(vp2D[i].x);
+    const size_t r = std::round(vp2D[i].y);
+    if (r >= 0 && r < imD_original.size[0] && c >= 0 &&
+        c < imD_original.size[1])
+      imD_aligned.at<ushort>(r, c) = imD_original.at<ushort>(vpPixelIdx[i].first, vpPixelIdx[i].second);
   }
 }
 
-int main(int argc, char **argv) {
-  if (argc < 3 || 4 < argc) {
+int main(int argc, char **argv)
+{
+  if (argc < 3 || 4 < argc)
+  {
     std::cout << "ERROR: please provide dataset path and output path"
               << std::endl;
     exit(1);
@@ -225,11 +211,15 @@ int main(int argc, char **argv) {
 
   std::string dataset_path, output_path;
   bool visualization = false;
-  try {
+  try
+  {
     dataset_path = argv[1];
     output_path = argv[2];
-    if (argc == 4) visualization = static_cast<bool>(std::stoi(argv[3]));
-  } catch (const std::exception &e) {
+    if (argc == 4)
+      visualization = static_cast<bool>(std::stoi(argv[3]));
+  }
+  catch (const std::exception &e)
+  {
     std::cerr << e.what() << std::endl;
   }
 
@@ -238,34 +228,44 @@ int main(int argc, char **argv) {
   LoadImages(dataset_path + "TIMESTAMP.txt", vstrRGBImageFilenames,
              vstrDepthImageFilenames, vTimestamps);
 
-  cv::Mat R = R_c_d.t();
-  cv::Mat t = -R * t_c_d;
+  cv::Mat R = R_c_d;
+  cv::Mat t = t_c_d;
 
   std::vector<cv::Mat> vp3D;
   std::vector<std::pair<size_t, size_t>> vpPixelIdx;
   std::vector<cv::Point2f> vp2D;
-  ProjectColorToDepth(K_c, K_d, dist_coeffs, R, t, vp3D, vpPixelIdx, vp2D);
 
-  for (const auto &strName : vstrDepthImageFilenames) {
-    cv::Mat imD, imD_aligned;
-    std::cout <<"File name: " <<strName << std::endl;
+  for (const auto &strName : vstrDepthImageFilenames)
+  {
+    cv::Mat imD, imD_aligned, img_filtered;
+    std::cout << "File name: " << strName << std::endl;
     imD = cv::imread(dataset_path + "depth/" + strName,
                      CV_LOAD_IMAGE_ANYCOLOR | CV_LOAD_IMAGE_ANYDEPTH);
-    AlignDepth(imD, imD_aligned, vp3D, vpPixelIdx, vp2D);
-    cv::imwrite(output_path + strName, imD_aligned);
 
-    if (visualization) {
+    ProjectDepthToColor(imD, K_c, K_d, dist_coeffs, R, t, vp3D, vpPixelIdx, vp2D);
+    AlignDepth(imD, imD_aligned, vp3D, vpPixelIdx, vp2D);
+
+    // Filtering because rounding is used, interpolation could be more accurate
+    imD_aligned.convertTo(imD_aligned, CV_32F);
+    cv::medianBlur(imD_aligned, img_filtered, 5);
+    img_filtered.convertTo(img_filtered, CV_16UC1);
+
+    cv::imwrite(output_path + strName, img_filtered);
+
+    if (visualization)
+    {
       cv::Mat dst, color, color_imD;
       color = cv::imread(dataset_path + "color/" + strName,
                          CV_LOAD_IMAGE_ANYCOLOR | CV_LOAD_IMAGE_ANYDEPTH);
 
-      imD_aligned.convertTo(color_imD, CV_32F);
-      color_imD /= 9;
+      img_filtered.convertTo(color_imD, CV_32F);
+      color_imD /= 8;
       color_imD.convertTo(color_imD, CV_8UC1);
       cv::applyColorMap(color_imD, color_imD, cv::COLORMAP_JET);
       cv::addWeighted(color, 0.6, color_imD, 0.4, 0.0, dst);
       cv::imshow("Blend", dst);
-      if (cv::waitKey(1) >= 0) continue;
+      if (cv::waitKey(1) >= 0)
+        continue;
     }
   }
 
